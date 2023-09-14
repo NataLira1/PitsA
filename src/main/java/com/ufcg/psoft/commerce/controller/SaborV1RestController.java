df160c98022137f085ea5f11ebbbfed65c46d4c8
122210188 JOSE JARDEL ALVES DE MEDEIROS <jose.jardel.alves.medeiros@lcc1-39.lcc.ufcg.edu.br>
2023-09-14 16:02:34 -0300

    US6, lcc1 14/09

---
+++ src/main/java/com/ufcg/psoft/commerce/controller/SaborV1RestController.java
@@ -0,0 +1,12 @@
+package com.ufcg.psoft.commerce.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/sabores")
+public class SaborV1RestController {
+
+
+
+}
