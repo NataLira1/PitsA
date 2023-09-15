a560141cf2361cea8f2e273703f17b9dd3b77697
NataLira1 <cavalcantenata28@gmail.com>
2023-09-15 10:04:36 -0300

    iniciando

---
+++ src/main/java/com/ufcg/psoft/commerce/models/Cliente.java
@@ -0,0 +1,19 @@
+package com.ufcg.psoft.commerce.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+
+    private String nomeCompleto;
+
+
+
+
+}
