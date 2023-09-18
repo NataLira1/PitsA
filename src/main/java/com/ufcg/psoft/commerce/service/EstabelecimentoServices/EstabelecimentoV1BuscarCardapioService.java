ed9c69e807735c10d751ae9fb48d09f76e1772f3
Jose-Jardel-Alves <jose.jardel.alves70@gmial.com>
2023-09-18 19:19:43 -0300

    testes de cardápio

--- src/main/java/com/ufcg/psoft/commerce/service/EstabelecimentoServices/EstabelecimentoV1BuscarCardapioService.java
+++ src/main/java/com/ufcg/psoft/commerce/service/EstabelecimentoServices/EstabelecimentoV1BuscarCardapioService.java
@@ -1,17 +1,18 @@
 package com.ufcg.psoft.commerce.service.EstabelecimentoServices;
 
-import com.ufcg.psoft.commerce.exception.EstabelecimentoCodigoAcessoInvalidoException;
-import com.ufcg.psoft.commerce.exception.EstabelecimentoNaoEncontradoException;
-import com.ufcg.psoft.commerce.models.Estabelecimento;
-import com.ufcg.psoft.commerce.models.Sabor;
-import com.ufcg.psoft.commerce.repositories.EstabelecimentoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ufcg.psoft.commerce.exception.EstabelecimentoCodigoAcessoInvalidoException;
+import com.ufcg.psoft.commerce.exception.EstabelecimentoNaoEncontradoException;
+import com.ufcg.psoft.commerce.exception.TipoInexistenteException;
+import com.ufcg.psoft.commerce.models.Estabelecimento;
+import com.ufcg.psoft.commerce.models.Sabor;
+import com.ufcg.psoft.commerce.repositories.EstabelecimentoRepository;
 
 @Service
 public class EstabelecimentoV1BuscarCardapioService implements EstabelecimentoBuscarCardapioService{
@@ -34,6 +35,10 @@ public class EstabelecimentoV1BuscarCardapioService implements EstabelecimentoBu
         Estabelecimento es = estabelecimentoRepository.findById(id).orElseThrow(EstabelecimentoNaoEncontradoException::new);
 
         if(!codigo.equals(es.getCodigoAcesso())) throw new EstabelecimentoCodigoAcessoInvalidoException();
+
+        if(!tipo.toUpperCase().equals("SALGADO") && !tipo.toUpperCase().equals("DOCE")){
+            throw new TipoInexistenteException();
+        }
 
         Stream<Sabor> cardapioFiltrado = es.getCardapio().stream().filter(
                 item -> item.getTipo() == tipo
